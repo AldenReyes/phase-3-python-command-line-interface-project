0de108d03c36bfea20457e258ae24dd47badb484
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-09-03 18:31:12 -0700

    Initializes add_book_menu and view_book_menu

--- lib/user_menus.py
+++ lib/user_menus.py
@@ -1,6 +1,9 @@
 import typer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from simple_term_menu import TerminalMenu
+from prettytable import PrettyTable
+
 from models import User, Book
 
 pp = typer.echo
@@ -9,18 +12,52 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-def add_book_menu(current_user):
+def add_book_menu():
     current_book = Book()
-    pp(f"{current_user}'s Virtual Bookshelf")
-    pp(" ")
 
-    title = typer.prompt("Enter book title", type=str)
-    current_book.title = title
-    author = typer.prompt("Enter book author", type=str)
-    current_book.author = author
-    year_published = typer.prompt("Enter book year published", type=int)
-    current_book.year_published = year_published
+    entry_items = [
+        "[1] Add to bookshelf",
+        "[2] Return to main menu",
+    ]
+    terminal_menu = TerminalMenu(entry_items, title="Enter an option below")
+    menu_entry_index = terminal_menu.show()
+    choice = menu_entry_index
 
-    session.add(current_book)
-    session.commit()
-    return
+    if choice == 0:
+        title = typer.prompt("Enter book title", type=str)
+        current_book.title = title
+        author = typer.prompt("Enter book author", type=str)
+        current_book.author = author
+        year_published = typer.prompt("Enter book year published", type=int)
+        current_book.year_published = year_published
+
+        session.add(current_book)
+        session.commit()
+        pp("Book has been added to shared bookshelf")
+        add_book_menu()
+
+    if choice == 1:
+        return
+
+
+def view_book_menu():
+    entry_items = [
+        "[1] View all books",
+        "[2] Return to main menu",
+    ]
+    terminal_menu = TerminalMenu(entry_items, title="Enter an option below")
+    menu_entry_index = terminal_menu.show()
+    choice = menu_entry_index
+
+    if choice == 0:
+        table = PrettyTable()
+        table.field_names = ["Title", "Author", "Year Published"]
+
+        books = tuple(book for book in session.query(Book))
+        for book in books:
+            table.add_row([book.title, book.author, book.year_published])
+        pp(table)
+        view_book_menu()
+
+    if choice == 1:
+        return
